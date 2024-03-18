c9fff6f16410a3ef4748a5b348a08d4c6ddf5890
Bogdan Ionita <bogdan.ionita96@gmail.com>
2024-03-18 22:01:49 +0200

    Revert to FILL_ME_IN
    Signed-off-by: Bogdan Ionita <bogdan.ionita96@gmail.com>

--- components/firebase/firebase.ts
+++ components/firebase/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { /* connectFirestoreEmulator, */ getFirestore } from 'firebase/firestore';
-// import { /* connectStorageEmulator, */ getStorage } from 'firebase/storage';
+import { /* connectStorageEmulator, */ getStorage } from 'firebase/storage';
 // import { isDev } from '../isDev';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,19 +9,19 @@ import { /* connectFirestoreEmulator, */ getFirestore } from 'firebase/firestore
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBdj1BzVT-4Sf163Y4aNMSSVKnN_yHpudY",
-    authDomain: "miniext-challenge-6efe4.firebaseapp.com",
-    projectId: "miniext-challenge-6efe4",
-    storageBucket: "miniext-challenge-6efe4.appspot.com",
-    messagingSenderId: "340605091743",
-    appId: "1:340605091743:web:511c43a748e2396535c7aa"
+    apiKey: "FILL_ME_IN",
+    authDomain: "FILL_ME_IN",
+    projectId: "FILL_ME_IN",
+    storageBucket: "FILL_ME_IN",
+    messagingSenderId: "FILL_ME_IN",
+    appId: "FILL_ME_IN"
 };
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 
 export const firestore = getFirestore(firebaseApp);
-export const baseBucketName = 'miniext-challenge-6efe4.appspot.com';
+export const baseBucketName = 'FILL_ME_IN';
 
 /* if (isDev) {
     connectFirestoreEmulator(firestore, '127.0.0.1', 8081);
